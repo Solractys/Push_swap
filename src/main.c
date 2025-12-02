d9a546413e57b32f3a3cfe1f18d139efab3fc9da
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-12-01 22:21:51 -0300

    3 numbers

--- src/main.c
+++ src/main.c
@@ -6,12 +6,12 @@
 /*   By: csilva-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 07:28:04 by csilva-s          #+#    #+#             */
-/*   Updated: 2025/11/29 22:48:41 by csilva-s         ###   ########.fr       */
+/*   Updated: 2025/12/01 22:21:24 by csilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
+// CRUD STACK
 t_stack    *create(int number)
 {
 	t_stack    *node;
@@ -47,6 +47,7 @@ void	show_stack(t_stack *node)
         temp = temp->next;
     }
 }
+// MOVES
 void	swap(t_stack **stack)
 {
 	if (stack == NULL)
@@ -121,51 +122,124 @@ void	reverse_rotate_both(t_stack **stack_A, t_stack **stack_B)
 	reverse_rotate(stack_A);
 	reverse_rotate(stack_B);
 }
-
-int	main(void)
+// ERROR HANDLING
+int	validate_input(char *inputs)
 {
-    // STACK CREATE
-	t_stack    *stack = NULL;
-	push(&stack, 1);
-	push(&stack, 2);
-	push(&stack, 3);
+	int y = 0;
+
+	if (inputs == NULL || inputs[0] == '\0')
+			return (0);
+		while (inputs[y])
+		{
+			if (inputs[y] == '+' || inputs[y] == '-')
+			{
+				y++;
+				continue ;
+			}
+
+			if (!ft_isdigit(inputs[y]))
+				return (0);
+			y++;
+		}
+	return (1);
+}
+
+t_stack	*parse_values(char **input)
+{
+	int x = 1;
+	t_stack *stack = NULL;
+	while (input[x])
+	{
+		push(&stack, ft_atoi(input[x]));
+		x++;
+	}
+	return (stack);
+}
+int	check_three(t_stack **stack)
+{
+	int	top = (*stack)->number;
+	int	mid = (*stack)->next->number;
+	int down = (*stack)->next->next->number;
+
+	if (top < mid && top < down)
+		return (1);
+	else
+		return (0);
+}
+
+void	sort_three(t_stack **stack)
+{
+	int top, mid, down = 0;
+	top = (*stack)->number;
+	mid = (*stack)->next->number;
+	down = (*stack)->next->next->number;
+
+	if (top > mid && top > down)
+		rotate(stack);
+	else if (top > mid && top < down)
+		swap(stack);
+	else if (top < mid && top < down)
+		return ;
+}
+
+void sort_func(t_stack **stack)
+{
+	while (check_three(stack) == 0)
+			sort_three(stack);
+}
+
+int	main(int argc, char **argv)
+{
+	int x = 1;
+	if (argc < 2)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return (0);
+	}
+	while (argv[x] != NULL)
+	{
+		if(!validate_input(argv[x]))
+		{
+			ft_putstr_fd("Error\n", 2);
+			return (0);
+		}
+		x++;
+	}
+	
+	t_stack *stack = NULL;
+	stack = parse_values(argv);
 	show_stack(stack);
-    // SWAP MOVE
-	ft_printf("\nSA :");
-	swap(&stack);
+	ft_printf("\n");
+	sort_func(&stack);
 	show_stack(stack);
+
 
     // PUSH MOVE
-	t_stack    *stack_A = NULL;
-	t_stack    *stack_B = NULL;
-	push(&stack_A, 1);
-	push(&stack_A, 2);
-	push(&stack_A, 3);
-
-	push(&stack_B, 4);
-	push(&stack_B, 5);
-	ft_printf("PA MOVE:\n");
-	show_stack(stack_A);
-	ft_printf("\n");
-	show_stack(stack_B);
-
-	ft_printf("AFTER PA MOVE:\n");
-	push_to(&stack_A, &stack_B);
-	show_stack(stack_A);
-	ft_printf("\n");
-	show_stack(stack_B);
-
-    //ROTATE MOVE
-	ft_printf("ROTATE MOVE: \n");
-	show_stack(stack);
-	ft_printf("AFTER \n");
-	rotate(&stack);
-	show_stack(stack);
-    // REVERSE ROTATE MOVE
-	ft_printf("REVERSE ROTATE MOVE: \n");
-	show_stack(stack);
-	reverse_rotate(&stack);
-	ft_printf("AFTER \n");
-	show_stack(stack);
+// 	t_stack    *stack_B = NULL;
+// 	push(&stack_B, 4);
+// 	push(&stack_B, 5);
+// 	ft_printf("PA MOVE:\n");
+// 	show_stack(stack);
+// 	ft_printf("\n");
+// 	show_stack(stack_B);
+// 
+// 	ft_printf("AFTER PA MOVE:\n");
+// 	push_to(&stack, &stack_B);
+// 	show_stack(stack);
+// 	ft_printf("\n");
+// 	show_stack(stack_B);
+// 
+//     //ROTATE MOVE
+// 	ft_printf("ROTATE MOVE: \n");
+// 	show_stack(stack);
+// 	ft_printf("AFTER \n");
+// 	rotate(&stack);
+// 	show_stack(stack);
+//     // REVERSE ROTATE MOVE
+// 	ft_printf("REVERSE ROTATE MOVE: \n");
+// 	show_stack(stack);
+// 	reverse_rotate(&stack);
+// 	ft_printf("AFTER \n");
+// 	show_stack(stack);
     return (0);
 }
